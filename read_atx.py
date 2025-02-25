a7d01a60329afe3ba2b7e448c8323c898454a06f
Markus Fritze <github@sarnau.com>
2025-02-25 13:23:42 +0100

    Update read_atx.py

--- read_atx.py
+++ read_atx.py
@@ -1,116 +1,136 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
-import os
 import struct
 
-def loadATX(atxFilename,selectedTrack=None):
+
+def parseATX(atxFilename):
 	data = bytearray(open(atxFilename,'rb').read())
 	SECTOR_SIZE = 128
 
-	offset = 0
+	CREATOR = {
+		0x01:'TX_CR_FX7',
+		0x02:'ATX_CR_FX8',
+		0x03:'ATX_CR_ATR',
+		0x10:'ATX_CR_WH2PC',
+		0x74:'a8diskutil',
+	}
+	DENSITY = ['SINGLE','MEDIUM','DOUBLE']
 
-	# read file header
-	header,version_number,fh_size = struct.unpack('<4sh22xl16x', data[offset+0:offset+0x30])
-	if header != b'AT8X':
+	fh_header,fh_version,fh_min_version,fh_creator,fh_creator_version,fh_flags,fh_image_type,fh_density,fh_start,fh_end = struct.unpack('<4shhhhLhB9xll12x', data[:0x30])
+	fh_header = fh_header.decode('ascii')
+#	print('$%08x : FILE HEADER %s VERSION:%d CREATOR:%s,VERSION:%d FLAGS:$%08x IMAGE_TYPE:$%04x DENSITY:%s / $%08x-$%08x' % (0, fh_header,fh_version,CREATOR[fh_creator],fh_creator_version,fh_flags,fh_image_type,DENSITY[fh_density],fh_start,fh_end))
+	if fh_header != 'AT8X':
 		print('File Header AT8X not found')
 		return False
-	if fh_size != 48:
-		print('File Header Size != 48 bytes')
-		return False
-	if version_number != 1:
+	if fh_version != 1:
 		print('File Header Version != 1')
 		return False
-	offset += fh_size
 
-	while offset < len(data):
-		# read track header
-		th_record_size,record_type,track_number,sector_count,th_size = struct.unpack('<lh2xB1xh8xl8x', data[offset+0:offset+0x20])
-		if th_size != 32:
-			print('Track Header Size != 32 bytes')
-			return False
-		if record_type != 0:
-			print('Track Header record type != data track')
-			return False
-		if not selectedTrack or (selectedTrack and selectedTrack == track_number):
-			track = data[offset+0:offset+th_record_size]
-			sector_lookup = {}
+	atx_offset = fh_start
+	while atx_offset < fh_end:
+		# data record header
+		dr_length,dr_type = struct.unpack('<lh2x', data[atx_offset:atx_offset+8])
+#		print('$%08x : DATA RECORD HEADER $%08x / $%04x' % (atx_offset, dr_length,dr_type))
+		if dr_type == 0x0100: # host data record
+			pass # ignored
+		elif dr_type == 0x0000: # track data record
+			th_track_number,th_sector_count,th_rate,th_flags,th_header_size = struct.unpack('<B1xhh2xll8x', data[atx_offset+8:atx_offset+8+24])
+#			print('$%08x : TRACK DATA RECORD TRACK:%2d SECTOR_COUNT:%2d RATE:%d FLAGS:$%08x HEADER_SIZE:$%08x' % (atx_offset+8, th_track_number,th_sector_count,th_rate,th_flags,th_header_size))
 
-			# read sector list header
-			record_size,record_type = struct.unpack('<lB3x', track[th_size+0:th_size+8])
-			if record_type != 1:
-				print('Sector List Header record type != sector list')
-				return False
+			sector_lookup = {} # find duplicate sectors
+			
+			coffset = atx_offset+th_header_size
+			while coffset < atx_offset+dr_length:
+				chunk_length,chunk_type,chunk_sector_index,chunk_header_data = struct.unpack('<lBBH', data[coffset:coffset+8])
+				if chunk_length == 0: # chunk list terminator
+					break
+				chunk_data = data[coffset+8:coffset+chunk_length]
+#				print('$%08x : CHUNK RECORD LENGTH:$%08x TYPE:$%02x SECTOR_INDEX:%2d HEADER_DATA:$%04x' % (coffset, chunk_length,chunk_type,chunk_sector_index,chunk_header_data))
+				if chunk_type == 0x00: # Sector data
+					sector_data = chunk_data # content of the sector data itself
+				elif chunk_type == 0x01: # Sector list
+					for soffset in range(coffset+8, coffset+chunk_length,8):
+						sl_sector_number,sl_sector_status,sl_sector_position,sl_start_data = struct.unpack('<BBHL', data[soffset:soffset+8])
 
-			# read sector list
-			for sectorHeaderOffset in range(th_size+8,th_size+record_size,8):
-				sector_number,sector_status,sector_position,start_data = struct.unpack('<BBhl', track[sectorHeaderOffset+0:sectorHeaderOffset+8])
+						status = []
+						if sl_sector_status == 0x00:
+							status.append('OK')
+						else:
+							if sl_sector_status & 0x80: # NOT READY - not valid/useful in the file
+								status.append('Reserved:80')
+							if sl_sector_status & 0x40:
+								status.append('EXTENDED DATA')
+							if sl_sector_status & 0x20:
+								status.append('deleted DAM')
+							if sl_sector_status & 0x10:
+								status.append('RECORD NOT FOUND')
+							if sl_sector_status & 0x08:
+								status.append('CRC ERROR')
+							if sl_sector_status & 0x04:
+								status.append('LOST DATA')
+							if sl_sector_status & 0x02: # DRQ - not valid/useful in the file
+								status.append('Reserved:02')
+							if sl_sector_status & 0x01: # BUSY - not valid/useful in the file
+								status.append('Reserved:01')
+						status_str = ','.join(status)
 
-				status = []
-				if sector_status == 0x00:
-					status.append('OK')
-				else:
-					if sector_status & 0x80: # NOT READY - not valid/useful in the file
-						status.append('Reserved:80')
-					if sector_status & 0x40:
-						status.append('EXTND DATA')
-					if sector_status & 0x20:
-						status.append('deleted DAM')
-					if sector_status & 0x10:
-						status.append('RECORD NOT FOUND')
-					if sector_status & 0x08:
-						status.append('CRC ERROR')
-					if sector_status & 0x04:
-						status.append('LOST DATA')
-					if sector_status & 0x02: # DRQ - not valid/useful in the file
-						status.append('Reserved:02')
-					if sector_status & 0x01: # BUSY - not valid/useful in the file
-						status.append('Reserved:01')
-				sstr = ','.join(status)
+						#print('$%08x : SECTOR HEADER NUMBER:%2d STATUS:%s POSITION:$%04x START:$%08x' % (soffset, sl_sector_number,status_str,sl_sector_position,sl_start_data))
 
-				# read sector data as well
-				sno = track_number * 18 + sector_number
-				spos = sector_position / 26042
-				print('Sector @ $%04x #%3d, Track #%2d Sector #%2d / %s / %7.3f%%' % (start_data, sno, track_number, sector_number,sstr,spos),end='')
+						# read sector data as well
+						sno = th_track_number * 18 + sl_sector_number
+						spos = sl_sector_position / 26042
+						print('SECTOR @ $%08x #%3d, Track #%2d Sector #%2d / %s / %7.3f%%' % (sl_start_data, sno, th_track_number, sl_sector_number,status_str,spos),end='')
 
-				if True:
-					sdata = track[start_data:start_data+SECTOR_SIZE]
-					lc = sdata[0]
-					for i in range(1,SECTOR_SIZE):
-						if lc != sdata[i]:
-							lc = -1
-							break
-					if lc >= 0:
-						print(' / $%02x * %d' % (lc, SECTOR_SIZE))
-					else:
-						foundDup = False
-						for sd_start in sector_lookup:
-							if sector_lookup[sd_start] == sdata:
-								print(' / sector copy @ $%04x' % (sd_start))
-								foundDup = True
-								break
-						sector_lookup[start_data] = sdata
-						if not foundDup:
+						if (sl_sector_status & 0x04) == 0x00: # data actually present?
+							sl_data = data[atx_offset+sl_start_data:atx_offset+sl_start_data+SECTOR_SIZE]
+							lc = sl_data[0]
+							for i in range(1,SECTOR_SIZE):
+								if lc != sl_data[i]:
+									lc = -1
+									break
+							if lc >= 0:
+								print(' / $%02x * %d' % (lc, SECTOR_SIZE))
+							else:
+								foundDup = False
+								for sd_start in sector_lookup:
+									if sector_lookup[sd_start] == sl_data:
+										print(' / sector copy @ $%04x' % (sd_start))
+										foundDup = True
+										break
+								sector_lookup[sl_start_data] = sl_data
+								if not foundDup:
+									print()
+									if False:
+										for l in range(0,SECTOR_SIZE,16):
+											print('%02x: ' % l,end='')
+											for p in range(0,16):
+												print('%02x ' % sl_data[l + p],end='')
+											print(' ',end='')
+											for p in range(0,16):
+												c = sl_data[l + p]
+												if c < 0x20 or c >= 0x7F:
+													c = ord('.')
+												print('%c' % c,end='')
+											print()
+						else:
 							print()
-							for l in range(0,SECTOR_SIZE,16):
-								print('%02x: ' % l,end='')
-								for p in range(0,16):
-									print('%02x ' % sdata[l + p],end='')
-								print(' ',end='')
-								for p in range(0,16):
-									c = sdata[l + p]
-									if c < 0x20 or c >= 0x7F:
-										c = ord('.')
-									print('%c' % c,end='')
-								print()
-				else:
-					print()
 
-			print('=' * 40)
+					pass
+				elif chunk_type == 0x10: # Weak sector data
+					print('WEAK_SECTOR #%2d STARTING_AT:$%02x' % (chunk_sector_index,chunk_header_data))
+				elif chunk_type == 0x11: # Extended sector data
+					print('LONG_SECTOR #%2d SECTOR_SIZE:%d' % (chunk_sector_index,128 << chunk_header_data))
+				else:
+					print('### UNKNOWN CHUNK TYPE $%02x' % chunk_type)
+					pass
+				coffset += chunk_length
+				
+		print('=' * 40)
 		# proceed to the next track
-		offset += th_record_size
+		atx_offset += dr_length
 
 	return True
 
-loadATX('Zorro.atx')
+
+parseATX('Zorro.atx')
